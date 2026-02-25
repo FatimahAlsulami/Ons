10f0bf2e9cd16707eead184e2098fd97853acc83
Fatima Alsulami <89698150+FatimahAlsulami@users.noreply.github.com>
2026-02-25 03:41:24 +0300

    Delete ons-web directory

--- ons-web/src/components/Icon3D.jsx
+++
@@ -1,7 +0,0 @@
-export default function Icon3D({ src, alt="", size=28, children }){
-  return (
-    <span className="icon3d" style={{ width:size, height:size }}>
-      {children ? children : <img src={src} alt={alt} />}
-    </span>
-  );
-}
